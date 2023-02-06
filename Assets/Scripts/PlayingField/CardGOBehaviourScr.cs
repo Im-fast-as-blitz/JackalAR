ac714a490c564651076f05d5fac02dee7e2a2320
Rodion Chernomordin <rodion.chernomordin@gmail.com>
2023-02-06 20:16:33 +0300

    Remove func

--- Assets/Scripts/PlayingField/CardGOBehaviourScr.cs
+++ Assets/Scripts/PlayingField/CardGOBehaviourScr.cs
@@ -3,14 +3,6 @@ using UnityEngine;
 
 public class CardGOBehaviourScr : MonoBehaviour {
     
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Person"))
-        {
-            Open();
-        }
-    }
-
     public void UpdateLogo()
     {
         CardGOInfo gOInfo = GetComponent<CardGOInfo>();
